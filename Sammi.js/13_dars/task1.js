eab7092e5e45ef78c1fc883cb6fb766acdb65f7e
Firdavs Nazarov <fredrickuzb55@gmail.com>
2023-10-02 10:50:03 +0900

    if,else,switch case

--- Sammi.js/13_dars/task1.js
+++ Sammi.js/13_dars/task1.js
@@ -1,10 +1,25 @@
 
-const age = +prompt('How old are you?','');
+// const age = +prompt('How old are you?','');
 
-if (age > 25){
-    console.log('Horror films');
-} else if (age >= 15){
-    console.log('25+ FILMS');
-} else{
-    console.log('Cartoon');
+// if (age > 25){
+//     console.log('Horror films');
+// } else if (age >= 15){
+//     console.log('25+ FILMS');
+// } else{
+//     console.log('Cartoon');
+// }
+
+
+
+// const age = +prompt('How old are you?','');
+const age= 25
+
+
+switch(age) {
+    case 20: 
+      console.log("Uylan so'ta qo'zi");
+      break;
+    case 25: 
+      console.log("Uylan endii");
+      break;
 }
