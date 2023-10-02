3c964386290afa9969ee38257143f7356537c120
Firdavs Nazarov <fredrickuzb55@gmail.com>
2023-10-02 12:09:45 +0900

    while/do,while/for_loop

---
+++ Sammi.js/14_dars/task.js
@@ -0,0 +1,23 @@
+ 
+
+
+
+ let startNum = 6;
+
+// while (startNum < 20) {
+//    console.log(startNum);
+//    startNum++;
+// }
+
+
+// do {
+//     console.log(startNum);
+//     startNum++
+// }while(startNum<10)
+
+
+for(i=10; i<20; i++){
+    console.log(i);
+}
+
+
