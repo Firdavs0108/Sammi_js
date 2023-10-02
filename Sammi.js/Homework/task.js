8fc9dad469614990ef77889cc76195a691a1e461
Firdavs Nazarov <fredrickuzb55@gmail.com>
2023-10-02 17:04:31 +0900

    homeWork

--- Sammi.js/Homework/task.js
+++ Sammi.js/Homework/task.js
@@ -4,13 +4,33 @@
 
 const numSerial = +prompt('How many serials do you watch in a week?')
 
-if(numSerial <= 5){
-    console.log('Less watcher');
-} else if (numSerial <= 10) {
-    console.log('Good watcher');
-}else{
-    console.log('Best watcher');
+// if(numSerial <= 5){
+//     console.log('Less watcher');
+// } else if (numSerial <= 10) {
+//     console.log('Good watcher');
+// }else{
+//     console.log('Best watcher');
+// }
+
+//Task1/2
+
+const dbSeries = {
+    series: {}
 }
 
-//Task1
+for (let i = 0; i<2; i++){
+    const   a = prompt("What is your last watched movie name"),
+            b = prompt("How much score can you give");
+        
+            if (a != null && b != null && a != "" && b != "") {
+                dbSeries.series [a] = b;
+                 console.log('Done');
+            } else {
+                console.log('ERROR, Try again');
+                i--
+            }
+                
+}
+
+console.log(dbSeries);
 
