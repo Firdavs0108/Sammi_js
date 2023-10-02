aa558df7b42fc124fffc869db1c4b449e306024a
Firdavs Nazarov <fredrickuzb55@gmail.com>
2023-10-02 10:41:27 +0900

    if_else

---
+++ Sammi.js/13_dars/task1.js
@@ -0,0 +1,10 @@
+
+const age = +prompt('How old are you?','');
+
+if (age > 25){
+    console.log('Horror films');
+} else if (age >= 15){
+    console.log('25+ FILMS');
+} else{
+    console.log('Cartoon');
+}
