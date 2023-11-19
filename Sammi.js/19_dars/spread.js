f654cef77774cf2a717a41c8de02c8171258d84e
Firdavs Nazarov <fredrickuzb55@gmail.com>
2023-11-19 23:57:32 +0900

    Sammi.js

---
+++ Sammi.js/19_dars/spread.js
@@ -0,0 +1,13 @@
+
+
+const numbers = {
+    x: 10,
+    y: 30,
+}
+
+const newNumers = {...numbers, z: 50}
+
+newNumers.g = "Salom";
+
+console.log(numbers);
+console.log(newNumers);
---
+++ Sammi.js/21_dars/OOP.js
@@ -0,0 +1,29 @@
+
+const car = {
+    motor: "X",
+    color: "red",
+    isAirbag: true,
+    isSpeed: function(number){
+       return number == 90 ? "Maximum speed" : "Please slow!!" 
+    }
+}
+
+// const exam = car
+
+const gm = {
+    // ...car,
+    isAirbag: false
+}
+
+
+const bmw = Object.create(car)
+
+console.log(bmw.isSpeed(90));
+
+// Object.setPrototypeOf(gm, car)
+// gm.__proto__ = car
+
+// gm = {...car}
+
+// console.log(gm.isSpeed(100));
+
