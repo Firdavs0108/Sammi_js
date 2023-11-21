9a78a0e2088aae83d5252f4c9ac80297c44241d8
Firdavs Nazarov <fredrickuzb55@gmail.com>
2023-11-21 21:25:46 +0900

    Sammi.js

--- Sammi.js/26_dars/js/script.js
+++ Sammi.js/26_dars/js/script.js
@@ -1,3 +1,14 @@
 'use strict'
 
+const background = document.querySelector('.promo__bg')
+
+background.style.background = "url('./img/1.jpg') center center / cover no-repeat";
+
+
+const promoImg = document.querySelectorAll('.promo__adv')
+
+promoImg.forEach((element) =>{
+    element.remove();
+})
+
 
