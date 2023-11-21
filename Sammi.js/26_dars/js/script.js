233812ceda86b08d2bd45c6cfb825530cd089a33
Firdavs Nazarov <fredrickuzb55@gmail.com>
2023-11-21 21:46:21 +0900

    Sammi.js

--- Sammi.js/26_dars/js/script.js
+++ Sammi.js/26_dars/js/script.js
@@ -4,11 +4,22 @@ const background = document.querySelector('.promo__bg')
 
 background.style.background = "url('./img/1.jpg') center center / cover no-repeat";
 
+const title = document.querySelector('.promo__genre')
 
-const promoImg = document.querySelectorAll('.promo__adv')
+title.textContent="COMEDY"
+
+const promoImg = document.querySelectorAll('.promo__adv img ')
 
 promoImg.forEach((element) =>{
     element.remove();
 })
 
+
+
+const serialList = document.querySelectorAll('.promo__interactive-item')
+
+serialList.forEach((input, index)=>{
+ return input[index]
+})
+ 
 
