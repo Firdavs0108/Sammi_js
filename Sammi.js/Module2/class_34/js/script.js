03742a2e296839a5c32fa4a095dae8fd04cf7371
Firdavs Nazarov <fredrickuzb55@gmail.com>
2023-11-28 22:42:25 +0900

    Sammi.js

--- Sammi.js/Module2/class_34/js/script.js
+++ Sammi.js/Module2/class_34/js/script.js
@@ -4,6 +4,17 @@ document.addEventListener('DOMContentLoaded', () => {
 const tabsParent = document.querySelector('.tabheader__items'),
     tabs = document.querySelectorAll('.tabheader__item'),
     tabsContent = document.querySelectorAll('.tabcontent')
+    loader = document.querySelector('.loader')
+
+    // Loader
+
+    setTimeout(() => {
+        loader.style.opacity = '0'
+        setTimeout(() => {
+            loader.style.display = 'none'
+        }, 500)
+    },2000)
+      
 
 function hideTabContents () {
     tabsContent.forEach((item) => {
