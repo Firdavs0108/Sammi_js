00d7a4eafdc43c397e5e12dac106cdb4ef41a1c3
Firdavs Nazarov <fredrickuzb55@gmail.com>
2023-11-30 00:27:46 +0900

    Sammi.js

--- Sammi.js/Module2/class_36/js/script.js
+++ Sammi.js/Module2/class_36/js/script.js
@@ -1,2 +1,21 @@
 
+ const box = document.querySelector('.box'),
+ btn = document.querySelector('button')
+
+//  const width = box.clientWidth
+//  const height = box.clientHeight
+
+
+// const width = box.offsetWidth
+// const height = box.offsetHeight
+
+
+const height = box.scrollHeight
+
+btn.addEventListener('click', () =>{
+    box.style.height = box.scrollHeight + 'px'
+})
  
+
+//  console.log(width);
+//  console.log(height);
