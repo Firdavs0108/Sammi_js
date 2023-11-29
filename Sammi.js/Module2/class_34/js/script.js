fbd3cd20ee574631cd73037be9cdb09dfd55f5d0
Firdavs Nazarov <fredrickuzb55@gmail.com>
2023-11-29 21:45:07 +0900

    Sammi.js

--- Sammi.js/Module2/class_34/js/script.js
+++ Sammi.js/Module2/class_34/js/script.js
@@ -51,11 +51,20 @@ tabsParent.addEventListener('click', (event) => {
 const deadline = '2024-11-24'
 
 function getTimeRemaining(endtime) {
-    const timer = Date.parse(endtime) - Date.parse(new Date()),
-    days = Math.floor(timer / (1000 * 60 * 60 * 24)),
-    hours = Math.floor((timer / (1000 * 60 * 60 )) % 24),
-    minutes =  Math.floor((timer / 1000 / 60 ) % 60),
-    seconds =  Math.floor((timer / 1000 ) % 60)
+    let days, hours, minutes, seconds
+    const timer = Date.parse(endtime) - Date.parse(new Date())
+    
+    if (timer <= 0) {
+        days = 0,
+        days = 0,
+        minutes = 0,
+        seconds = 0
+    } else{
+        days = Math.floor(timer / (1000 * 60 * 60 * 24)),
+        hours = Math.floor((timer / (1000 * 60 * 60 )) % 24),
+        minutes =  Math.floor((timer / 1000 / 60 ) % 60),
+        seconds =  Math.floor((timer / 1000 ) % 60)
+    }
 
     return {timer, days, hours, minutes, seconds}
 }
