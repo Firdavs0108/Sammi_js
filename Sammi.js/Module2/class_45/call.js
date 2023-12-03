203ea4b8ff9e4c2f4bcad64c9c76eb9f70a1d541
Firdavs Nazarov <fredrickuzb55@gmail.com>
2023-12-03 21:19:27 +0900

    apply.js

--- Sammi.js/Module2/class_45/call.js
+++ Sammi.js/Module2/class_45/call.js
@@ -16,3 +16,13 @@ logger.call(car,2023)                              //call
 logger.apply(car, [2023])                         //apply
 let binded = logger.bind(car, 2023)               //bind
 binded()
+
+const btn = document.querySelector('button')
+
+let currentWidth = 300;
+
+btn.addEventListener('click', function(){
+    const currentWidth = parseInt(this.style.width) || 100
+    this.style.width = currentWidth === 100 ? '300px' : '100px'
+});
+
