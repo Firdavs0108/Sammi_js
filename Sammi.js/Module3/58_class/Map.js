88e60332a64b2781f34c793bb7cabf5e2e4824a8
Firdavs Nazarov <fredrickuzb55@gmail.com>
2023-12-15 21:36:33 +0900

    map,some,every..js

---
+++ Sammi.js/Module3/58_class/Map.js
@@ -0,0 +1,42 @@
+
+
+
+//Map.......................................................
+
+
+// const cars = [ 'MerS', 'Bmw', 'KiA9']
+
+// const arr = cars.map((item) => item.toUpperCase())
+
+// console.log(arr);
+
+
+
+//Filter....................................................
+
+// const cars = ['mercedes benz', 'KiaMorning', 'audi', 'BMW']
+
+// const arr = cars.filter(item => item.length < 5 && item == item.toLowerCase())
+
+// console.log(arr);
+
+
+
+
+///Some.......................................................
+
+// const example = [ 1, 'name ', true]
+// console.log(example.some((item) => typeof item == 'number'));
+
+
+///Every.......................................................
+
+// const example = [1,'name', false]
+
+// console.log(example.every((item) => typeof item == 'String'));
+
+
+//Reduce........................................................
+
+// const numbers = [1,2,45,67,8]
+// console.log(numbers.reduce((sum, num) => sum + num + 20 ));
