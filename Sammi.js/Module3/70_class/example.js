2b0a1f9abe3a31804a96dac4110d044c64159834
Firdavs Nazarov <fredrickuzb55@gmail.com>
2023-12-23 00:34:26 +0900

    try_catch.js

---
+++ Sammi.js/Module3/70_class/example.js
@@ -0,0 +1,18 @@
+
+
+// import Car from "./g_setter.js";
+
+// const user1 = new Car('merc', 'black', 123)
+
+// user1._extraBonus = 234
+// user1.bonus = 59
+
+// user1.info()
+
+try {
+    console.log("hello");
+    console.log("Welcome");
+    console.log(a);
+} catch (error) {
+    console.log("error");
+}
